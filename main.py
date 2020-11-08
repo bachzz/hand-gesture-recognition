1c2ee2286d6cad9beb065779634b906b0e9517f3
bachng248 <ngb1998@gmail.com>
2020-11-08 13:44:17 +0700

    update

--- main.py
+++ main.py
@@ -13,28 +13,22 @@ model = model_from_json(json_file.read())
 model.load_weights("model_weights.h5")
 # model._make_predict_function()
 
-img_tmp = cv2.imread("./pics/A_test.jpg")
+img_tmp = cv2.imread("./pics/D1.jpg")
+# img_tmp = cv2.cvtColor(img_tmp, cv2.COLOR_BGR2GRAY)
+# img_tmp = cv2.resize(img_tmp, (28,28)) 
 img_tmp = cv2.resize(img_tmp, (64,64)) 
+# cv2.imshow('tmp',img_tmp)
 img_tmp = np.expand_dims(img_tmp,axis=0)
+# img_tmp = img_tmp.reshape(1,28,28,1)
 
 cap = cv2.VideoCapture(0)
 
 
 ##### MNIST #####
 
-# alphabet = {
-#     "A":0, "B":1, "C":2, "D":3, "E":4, "F": 5,
-#     "G":6, "H":7, "I":8, "K":10, "L":11, "M":12,
-#     "N":13, "O":14, "P":15, "Q":16, "R":17, "S": 18,
-#     "T":19, "U":20, "V":21, "W":22, "X":23, "Y":24
-# }
-
-# def get_key(val):
-#    for key, value in alphabet.items():
-#       print("hey",value)
-#       if val == value:
-#          return key
-#       return "key doesn't exist"
+# alphabet = np.array(["A","B","C","D","E","F","G","H","I","K","L","M",
+#             "N","O","P","Q","R","S","T","U","V","W","X","Y"])
+# print(len(alphabet))
 
 ########
 
@@ -51,19 +45,21 @@ while(True):
     # Capture frame-by-frame
     ret, img = cap.read()
     
-    x, y, w, h = 100, 100, 300, 300
+    x, y, w, h = 100, 100, 200, 200
 
     color = (255, 0, 0)
     thickness = 2
 
+    # img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     img = cv2.rectangle(img, (x,y), (x+w, y+h), color, thickness) 
 
     imgCrop = img[y:y+h, x:x+w]
+    # imgCrop = cv2.resize(imgCrop, (28,28)) 
     imgCrop = cv2.resize(imgCrop, (64,64)) 
     cv2.imshow('crop',imgCrop)
 
     imgCrop = np.expand_dims(imgCrop, axis=0)
-    
+    # imgCrop = imgCrop.reshape(1,28,28,1)
     
     # print(img_tmp.shape)
 
@@ -76,7 +72,7 @@ while(True):
 
     # print(pred)
     # print(pred[0])
-    # print(alphabet[pred[0]])
+    # print(np.argmax(pred))
     print(alphabet[np.argmax(pred)])
     # print(np.ma.masked_array(alphabet, pred[0]))
     # print(imgCrop)
