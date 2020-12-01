6958459c89dd72d8e72631d725eb195edbbe7ef5
Dat Nguyen <ntiendat.ict@gmail.com>
2020-12-01 16:46:22 +0700

    Fixed some warning and code refactorization

--- server.py
+++ server.py
@@ -15,15 +15,15 @@ app = Flask(__name__)
 CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-
-#with tf.compat.v1.Session() as sess:
+# with tf.compat.v1.Session() as sess:
 lb = pickle.load(open("lb.h5", "rb"))
 json_file = open("model.json", "r")
 model = model_from_json(json_file.read())
 model.load_weights("model_weights.h5")
 
-alphabet = np.array(["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"])
-
+alphabet = np.array(
+    ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W",
+     "X", "Y", "Z"])
 
 
 def convert_base64_to_image(image_base64):
@@ -34,29 +34,30 @@ def convert_base64_to_image(image_base64):
         return None
     return image_base64
 
+
 @app.route('/recognize', methods=['POST'])
 @cross_origin(origin='*')
 def hand_gesture_recognize():
     image_base64 = request.form.get('image')
 
     print(image_base64)
-#    image_base64 = convert_base64_to_image(image_base64)
-    image_base64 = np.fromstring(base64.b64decode(image_base64), dtype=np.uint8)
+    #    image_base64 = convert_base64_to_image(image_base64)
+    image_base64 = np.frombuffer(base64.b64decode(image_base64), dtype=np.uint8)
     print(image_base64)
     image_base64 = cv2.imdecode(image_base64, cv2.IMREAD_GRAYSCALE)
 
     print(image_base64.shape)
-    #Resize image to 28*28(maybe should let client do this part)
+    # Resize image to 28*28(maybe should let client do this part)
     image_base64 = cv2.resize(image_base64, (28, 28))
     image_base64 = image_base64.reshape((1, 28, 28, 1))
-#    with tf.compat.v1.Session() as sess:
+    #    with tf.compat.v1.Session() as sess:
     pred = model.predict(image_base64)
 
     print(pred)
-    print(alphabet[lb.inverse_transform(pred)[0]])
+    return_value = alphabet[lb.inverse_transform(pred)[0]]
 
-    return ("xin chao")
+    return (return_value)
 
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port='5000')
+    app.run(host='127.0.0.1', port='5000', debug=True)
